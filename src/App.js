c208bf1e079cf1963d7060e1b44b9dcebbad89de
Shubham <shubhambhutada7810@gmail.com>
2023-12-16 17:21:35 +0530

    form commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validConfirmPassword, setValidConfirmPassword] = useState(false);
+  
+  function validateEmail(e) {
+    setEmail(e.target.value);
+    if((e.target.value).includes("@gmail.com")) {
+      setValidEmail(true)
+    } else {
+      setValidEmail(false)
+    }
+  }
+
+  function validatePassword(e) {
+    setPassword(e.target.value);
+    if((e.target.value).length>=8) {
+      setValidPassword(true);
+    } else {
+      setValidPassword(false);
+    }
+  }
+
+  function validatConfirmPassword(e) {
+    setConfirmPassword(e.target.value);
+    if((e.target.value)===password) {
+      setValidConfirmPassword(true);
+      return true;
+    } else {
+      setValidConfirmPassword(false);
+      return false;
+    }
+  }
+
+  const inputEmailStyle = {
+    border: validEmail ? "2px solid green" : "2px solid red",
+  };
+  const inputPasswordStyle = {
+    border: validConfirmPassword ? "2px solid green" : "2px solid red",
+  };
+
+  function validateForm(e) {
+    e.preventDefault();
+    if(validConfirmPassword===false){
+      alert("Form cannot be submitted")
+    } else {
+      alert("Form submitted successfully!");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={validateForm} className="form-verification">
+      <div className="inputs">
+        <label htmlFor="email"><b>Email:</b></label> <br/>
+        <input type="email" id="email" onChange={validateEmail} value={email} style={inputEmailStyle} /> <br/>
+        {
+          validEmail===true? null : <p style={{color:"red"}}>Invalid email format</p> 
+        }
+      </div>
+      <div className="inputs">
+        <label htmlFor="password"><b>Password:</b></label> <br/>
+        <input type="password" id="password" onChange={validatePassword} value={password} style={inputPasswordStyle} /> <br/>
+        {
+          validPassword===true ? null : <p style={{color:"red"}}>Password must be atleast 8 characters</p>
+        }
+      </div>
+      <div className="inputs">
+        <label htmlFor="confirm-password"><b>Confirm Password:</b></label> <br/>
+        <input type="password" id="confirm-password" onChange={validatConfirmPassword} value={confirmPassword} style={inputPasswordStyle} /> <br/>
+        {
+          validConfirmPassword===true ? null : <p style={{color:"red"}}>Passwords do not match</p>
+        }
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  )
 }
 
 export default App;
